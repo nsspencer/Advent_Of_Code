5a18b0e9db28b871318d55ae3bad93c31e3b4016
Nathan Spencer <nathanspencer@cloudfitsoftware.com>
2023-12-07 21:49:12 -0500

    added part 2 for day 3

--- 2023/day_3/solve.py
+++ 2023/day_3/solve.py
@@ -9,6 +9,44 @@ import re
 INPUT_FILE = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
+class PartNumber:
+    def __init__(self, number:int, line_index:int, start:int, stop:int) -> None:
+        self.number = number
+        self.line_index = line_index
+        self.start = start
+        self.stop = stop
+
+    def contains_point(self, line_index:int, index:int) -> bool:
+        if line_index != self.line_index:
+            return False
+        if index >= self.start and index < self.stop:
+            return True
+        return False
+    
+    def __repr__(self) -> str:
+        return str(self.number)
+
+    def __add__(self, other):
+        if isinstance(other, PartNumber):
+            return PartNumber(self.number + other.number, None, None, None)
+        else:
+            raise TypeError("Unsupported operand type for +")
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, PartNumber):
+            return PartNumber(self.number * other.number, None, None, None)
+        elif isinstance(other, int):
+            return PartNumber(self.number * other, None, None, None)
+        else:
+            raise TypeError("Unsupported operand type for *")
+
+
 def generate_bounding_indices(row:int, start:int, stop:int) -> list:
     """Create a list of indices that create a bounding box around a string of characters.
     Only non-negative indices will be created. You are responsible for bounds checking.
@@ -38,7 +76,7 @@ def generate_bounding_indices(row:int, start:int, stop:int) -> list:
     return final_indices
 
 
-def check_adjacency(schematic:List[str], line_no:int, number_start_idx:int, number_end_index:int) -> bool:
+def check_character_adjacency(schematic:List[str], line_no:int, number_start_idx:int, number_end_index:int) -> bool:
     """Get the surrounding indices of the part.
 
     Args:
@@ -84,10 +122,48 @@ def parse_part_numbers(schematic:List[str]) -> list:
 
         # get all the part numbers that pass the adjacency check
         for start, stop in match_indices:
-            if check_adjacency(schematic, line_index, start, stop) == True:
-                part_numbers.append(int("".join(line[start:stop])))
+            if check_character_adjacency(schematic, line_index, start, stop) == True:
+                part_numbers.append(PartNumber(int("".join(line[start:stop])), line_index, start, stop))
 
     return part_numbers
+
+
+def calculate_gear_ratio(schematic:List[str], part_numbers:List[PartNumber]) -> int:
+    """Calculate gear ratio for the schematic and all the valid part numners in it.
+
+    Args:
+        schematic (List[str]): list of strings represeting the schematic.
+        part_numbers (List[PartNumber]): parsed and validated part numbers.
+
+    Returns:
+        int: value representing the gear ratio of the schematic.
+    """
+    gear_ratio = 0
+    for line_index, line in enumerate(schematic):
+        # find all the gears
+        gears = [match.start() for match in re.finditer(r"\*", line)]
+        if len(gears) == 0:
+            continue
+        
+        # get only the parts that exist on lines that can apply to this gear
+        possible_parts = [p for p in part_numbers if p.line_index in [line_index-1, line_index, line_index+1]]
+
+        # check the valid part numbers touching the gears
+        for gear_index in gears:
+            touching_part_numbers = set()
+            bounding_box = generate_bounding_indices(line_index, gear_index, gear_index+1)
+            for _line_number, _index in bounding_box:
+                for part in possible_parts:
+                    if part.contains_point(_line_number, _index):
+                        touching_part_numbers.add(part)
+            
+            if len(touching_part_numbers) == 2:
+                p1 = touching_part_numbers.pop()
+                p2 = touching_part_numbers.pop()
+                gear_ratio += p1 * p2
+    
+    return gear_ratio.number
+
 
 
 def main() -> int:
@@ -99,8 +175,10 @@ def main() -> int:
             schematic.append(line)        
 
     part_numbers = parse_part_numbers(schematic)
+    gear_ratio = calculate_gear_ratio(schematic, part_numbers)
 
     print(f"Part Number Sum: {sum(part_numbers)}")
+    print(f"Gear Ratio: {gear_ratio}")
     return 0
 
 
